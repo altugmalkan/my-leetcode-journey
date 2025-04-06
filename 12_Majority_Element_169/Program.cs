55ed2ef1f500fbdb8de0a84ea4776417af4bdf32
altugmalkan <altug33@windowslive.com>
2025-04-06 16:36:45 +0300

    feat: Implement 2 solutions for Majority Element problem

---
+++ 12_Majority_Element_169/Program.cs
@@ -0,0 +1,67 @@
+﻿class Program
+{
+    public static int MajorityElement(int[] nums)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            if (dict.ContainsKey(num))
+            {
+                dict[num] += 1;
+            }
+            else
+            {
+                dict[num] = 1;
+            }
+        }
+
+        int maxCount = 0;
+        int res = nums[0];
+
+        foreach (var pair in dict)
+        {
+            if (pair.Value > maxCount)
+            {
+                maxCount = pair.Value;
+                res = pair.Key;
+            }
+        }
+        return res;
+    }
+
+    public static int MajorityElement2(int[] nums)
+    {
+        int count = 0;
+        int value = 0;
+
+        foreach (var num in nums)
+        {
+            if (count == 0)
+            {
+                value = num;
+                count = 1;
+            }
+            else if (num == value)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return value;
+    }
+
+    public static void Main()
+    {
+        int[] nums = new int[] { 2, 2, 3, 3, 3, 3, 2 };
+
+        int result = MajorityElement2(nums);
+
+        Console.WriteLine(result);
+    }
+    
+
+}
