6277312ff251d3f0a0ac9dc011561374fe37b090
altugmalkan <altug33@windowslive.com>
2025-04-07 15:06:21 +0300

    feat: Implement a solution for Search Insert Position problem

---
+++ 13_Search_Insert_Position_35/Program.cs
@@ -0,0 +1,69 @@
+﻿class Program
+{
+    public static int SearchInsert(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            // int mid = (right +left) / 2; // This can cause overflow
+            if (nums[mid] == target)
+            {
+                return mid;
+
+
+            else if (target > nums[mid])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return left;
+        }
+    }
+
+    /* 
+    I wrote binary search to remember how it works
+    This is not needed for the solution, 
+    How ever you need to know how it works for you to solve the Search Insert problem
+    */
+    public static int BinarySearch(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                return target;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] nums = { 1, 3, 5, 6 };
+        int target = 0;
+        int result = SearchInsert(nums, target);
+        Console.WriteLine(result);
+    }
+}
